369d94f3e9e6b8e48361d1e20d2599b0e5881c03
Haiau Duong <duong.h.pgd@gmail.com>
2018-11-21 15:09:31 -0500

    added back button and routes to main menu

--- client/components/Credits.js
+++ client/components/Credits.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import MenuButton from './MenuButton'
 
 class Credits extends React.Component {
   render() {
     return (
       <div>
-        <div class="fade" />
+        {/* <div className="ReturnButton">
+          <div onClick={() => this.props.history.push('/')} class="backArrow">
+            &#10140;
+          </div>
+        </div> */}
+        <MenuButton />
 
-        <section class="star-wars">
-          <div class="crawl">
-            <div class="title">
+        <div className="fade" />
+
+        <section className="star-wars">
+          <div className="crawl">
+            <div className="title">
               <p>Episode I</p>
               <h1>LIGHT SPEED</h1>
             </div>
