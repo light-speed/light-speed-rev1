716a0ff4ab1416c2a81965c2b8b6d015b184838c
Haiau Duong <duong.h.pgd@gmail.com>
2018-11-21 12:16:00 -0500

    prettified menu

--- client/components/Menu.js
+++ client/components/Menu.js
@@ -1,23 +1,26 @@
 import React from 'react'
 
 class Menu extends React.Component {
-
-    render () {
-        return (
-            <div className='Menu'>
-                <h1>LIGHT SPEED</h1>
-                <div>
-                    <button onClick={() => this.props.history.push('/play')}>PLAY GAME</button>
-                </div>
-                <div>
-                    <button>HOW TO PLAY</button>
-                </div>
-                <div>
-                    <button onClick={() => this.props.history.push('/credits')}>CREDITS</button>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Menu">
+        <h1>LIGHT SPEED</h1>
+        <div>
+          <button onClick={() => this.props.history.push('/play')}>
+            PLAY GAME
+          </button>
+        </div>
+        <div>
+          <button>HOW TO PLAY</button>
+        </div>
+        <div>
+          <button onClick={() => this.props.history.push('/credits')}>
+            CREDITS
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default (Menu)
+export default Menu
