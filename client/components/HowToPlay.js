04bffc24e01b9c0741687796c6c2ef02bf87b520
Dalton Saffe <daltonsaffe@gmail.com>
2018-11-24 21:02:26 -0500

    ADD: how-to play

--- client/components/HowToPlay.js
+++ client/components/HowToPlay.js
@@ -7,6 +7,7 @@ class HowToPlay extends React.Component {
       <div>
         <MenuButton />
         <div className="HowToPlay" />
+        <img src="./images/instruc.png" />
       </div>
     )
   }
