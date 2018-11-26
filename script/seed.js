6515c476f151a4003ac495e805e83e6e63abe8e2
Dalton Saffe <daltonsaffe@gmail.com>
2018-11-26 13:15:29 -0500

    db game score associations

--- script/seed.js
+++ script/seed.js
@@ -26,24 +26,24 @@ async function seed() {
       password: 'saxophone'
     }),
     User.create({
-      email: 'asdef@email.com',
+      email: 'abmgef@email.com',
       username: 'BMG',
       password: 'saxophone'
     })
   ])
 
   const games = await Promise.all([
-    Game.create({score: 3200, userID: 1}),
-    Game.create({score: 5200, userID: 5}),
-    Game.create({score: 23900, userID: 2}),
-    Game.create({score: 93900, userID: 4}),
-    Game.create({score: 88700, userID: 4}),
-    Game.create({score: 18700, userID: 4}),
-    Game.create({score: 8700, userID: 5}),
-    Game.create({score: 8700, userID: 4}),
-    Game.create({score: 700, userID: 4}),
-    Game.create({score: 8700, userID: 3}),
-    Game.create({score: 98700, userID: 4})
+    Game.create({score: 3200, userId: 1}),
+    Game.create({score: 5200, userId: 5}),
+    Game.create({score: 23900, userId: 2}),
+    Game.create({score: 93900, userId: 4}),
+    Game.create({score: 88700, userId: 4}),
+    Game.create({score: 18700, userId: 4}),
+    Game.create({score: 8700, userId: 5}),
+    Game.create({score: 8700, userId: 4}),
+    Game.create({score: 700, userId: 4}),
+    Game.create({score: 8700, userId: 3}),
+    Game.create({score: 98700, userId: 4})
   ])
 
   console.log(`seeded ${users.length} users`)
