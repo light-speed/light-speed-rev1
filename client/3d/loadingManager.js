14fc1f684c289d419315ec9ec9cad0a64d0848e0
Dalton Saffe <daltonsaffe@gmail.com>
2018-11-27 17:12:05 -0500

    b/ fix loading bug

--- client/3d/loadingManager.js
+++ client/3d/loadingManager.js
@@ -8,14 +8,13 @@ export let RESOURCES_LOADED = false
 let resources = 0
 loadingManager.onProgress = () => resources++
 
-loadingManager.onLoad = function () {
+loadingManager.onLoad = function() {
   RESOURCES_LOADED = true
   LOADING_SCREEN.style.display = 'none'
   HUD.style.display = 'flex'
 
   // tell client/server to start the game
-  if (!store.getState().game.ongoing)
-    store.dispatch(startGame())
+  if (!store.getState().game.ongoing) store.dispatch(startGame())
 }
 
 export default loadingManager
