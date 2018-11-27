3bbe36d93917a1ea2cb498278ff5fc3722243ea8
Haiau Duong <duong.h.pgd@gmail.com>
2018-11-27 12:47:44 -0500

    main menu double columns

--- client/components/Menu.js
+++ client/components/Menu.js
@@ -7,32 +7,29 @@ class Menu extends React.Component {
         <div>
           <img src="./images/lightGif.gif" />
         </div>
+        
         <div>
           <button onClick={() => this.props.history.push('/play')}>
             Play Game
           </button>
-        </div>
-        <div>
-          <button onClick={() => this.props.history.push('/Login')}>
-            Login
-          </button>
-        </div>
-        <div>
-          <button onClick={() => this.props.history.push('/Signup')}>
-            Sign Up
-          </button>
-        </div>
-        <div>
           <button onClick={() => this.props.history.push('/howtoplay')}>
             How To Play
           </button>
         </div>
+
+        <div>
+          <button onClick={() => this.props.history.push('/Login')}>
+            Login
+          </button>
+          <button onClick={() => this.props.history.push('/Signup')}>
+            Sign Up
+          </button>
+        </div>
+
         <div>
           <button onClick={() => this.props.history.push('/topscore')}>
             Top Scores
           </button>
-        </div>
-        <div>
           <button onClick={() => this.props.history.push('/credits')}>
             Credits
           </button>
